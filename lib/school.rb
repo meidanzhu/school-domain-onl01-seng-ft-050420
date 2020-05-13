e5f3a426597687d11d2fee595f2fb43d7779912b
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-13 18:36:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,32 @@
 class School
   def initialize(name)
-   @name = name
-   @roster = {}
- end
+    @name = name
+    @roster = {}
+  end
 
- def roster
-   @roster
- end
+  def roster
+    @roster
+  end
 
- def add_student(name, grade)
-   if @roster[grade] != false
-        @roster[grade] << name
-      else
-        @roster[grade] = [name]
-      end
- end
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
 
- def grade(grade)
-   @roster[grade]
- end
+  def grade(grade)
+    @roster[grade]
+  end
 
- def sort
-   @roster.each do |key, value|
-     value.sort!
-   end
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
 end
-
+ 
 school = School.new
 school.add_student("Zach Morris", 9)
 school.roster
